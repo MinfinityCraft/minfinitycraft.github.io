b5412bc04114f0d1b12e83144873a456ab937edc
MinfinityCraft <yuzumikan773@gmail.com>
2026-03-07 23:41:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ fetch("header.html")
 .then(data => {
 document.getElementById("header").innerHTML = data;
 });
+
+function searchRecipe(){
+
+let input = document.getElementById("recipeSearch").value.toLowerCase();
+
+let recipes = document.querySelectorAll(".recipe-item");
+
+recipes.forEach(function(recipe){
+
+let keywords = recipe.dataset.keywords.toLowerCase();
+
+if(keywords.includes(input)){
+recipe.style.display="block";
+}else{
+recipe.style.display="none";
+}
+
+});
+
+}
