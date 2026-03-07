289cbdb450cf90657c21e9fef9209f1b62655eb7
MinfinityCraft <yuzumikan773@gmail.com>
2026-03-08 00:58:00 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,57 +1,36 @@
-const materials = [
+const recipes = [
 
-
-
-  
 {
-name: "鉄素材",
-mine: "第二",
-keywords: "鉄 第二採掘場 インゴット",
-items: ["鉄インゴット","鉄インゴット","鉄インゴット"]
+name:"鉄ピッケル",
+mine:"第一",
+keywords:"鉄 ピッケル 第一",
+items:["鉄素材","鉄素材","棒"]
 },
 
-
-
-  
 {
-name: "金素材",
-mine: "第一",
-keywords: "金 第一採掘場 インゴット",
-items: ["金インゴット","金インゴット","金インゴット"]
+name:"金ピッケル",
+mine:"第二",
+keywords:"金 ピッケル 第二 中級",
+items:["金素材","金素材","棒"]
 }
 
 ];
 
 
+const materials = [
 
-
-
-
-
-
-
-
-
-const recipes = [
-
-
-
-  
 {
-name: "金ピッケル",
-mine: "第二",
-keywords: "金 ピッケル 第二 中級",
-items: ["金素材","金素材","棒"]
+name:"鉄素材",
+mine:"第二",
+keywords:"鉄 第二採掘場 インゴット",
+items:["鉄インゴット","鉄インゴット","鉄インゴット"]
 },
 
-
-
-  
 {
-name: "鉄ピッケル",
-mine: "第一",
-keywords: "鉄 ピッケル 第一",
-items: ["鉄素材","鉄素材","棒"]
+name:"金素材",
+mine:"第一",
+keywords:"金 第一採掘場 インゴット",
+items:["金インゴット","金インゴット","金インゴット"]
 }
 
 ];
