0423b0b6fc8e4d54f9438d244579d92f096e619a
MinfinityCraft <yuzumikan773@gmail.com>
2026-03-08 15:14:11 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,54 +1,38 @@
 const pickaxeRecipes = [
 
 {
-name: "木のピッケル",
-recipe: [
+mine:"採掘場A",
+name:"木のピッケル",
+recipe:[
 "木材 ×3",
 "棒 ×2"
 ]
 },
 
 {
-name: "石のピッケル",
-recipe: [
+mine:"採掘場A",
+name:"石のピッケル",
+recipe:[
 "石 ×3",
 "棒 ×2"
 ]
 },
 
 {
-name: "鉄のピッケル",
-recipe: [
+mine:"採掘場B",
+name:"鉄のピッケル",
+recipe:[
 "鉄 ×3",
 "棒 ×2"
 ]
-}
-
-];
-
-
-const materialRecipes = [
-
-{
-name: "圧縮鉄",
-recipe: [
-"鉄 ×9"
-]
 },
 
 {
-name: "強化棒",
-recipe: [
-"木材 ×2",
-"石 ×1"
-]
-},
-
-{
-name: "魔力クリスタル",
-recipe: [
-"ダイヤ ×1",
-"レッドストーン ×4"
+mine:"採掘場B",
+name:"ダイヤピッケル",
+recipe:[
+"ダイヤ ×3",
+"棒 ×2"
 ]
 }
 
