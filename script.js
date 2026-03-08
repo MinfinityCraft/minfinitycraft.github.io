c6dcf3bb68b4c13978b4881ca0dcc5ad5eaead91
MinfinityCraft <yuzumikan773@gmail.com>
2026-03-08 18:05:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var acc = document.getElementsByClassName("accordion");
 for (var i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+    this.classList.toggle("active"); // 色とアイコン切り替え
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
@@ -12,7 +12,7 @@ for (var i = 0; i < acc.length; i++) {
   });
 }
 
-// ハンバーガーメニューの動作（既存コードと同じ）
+// ハンバーガーメニュー
 function toggleMenu() {
   document.querySelector(".side-header").classList.toggle("active");
 }
