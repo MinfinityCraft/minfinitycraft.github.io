4443433819be181ddae6c22380542a97d82b8dbd
MinfinityCraft <yuzumikan773@gmail.com>
2026-03-08 15:01:43 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,11 +1,21 @@
-// data.js
-const materialsRecipes = [
-  { name: "木の板", ingredients: ["木材 x1"], result: "木の板 x4" },
-  { name: "石の道具", ingredients: ["石 x3", "棒 x2"], result: "石のツルハシ x1" },
-  // どんどん追加
-];
-
 const pickaxeRecipes = [
-  { name: "木のツルハシ", ingredients: ["木材 x3", "棒 x2"], result: "木のツルハシ x1" },
-  { name: "石のツルハシ", ingredients: ["石 x3", "棒 x2"], result: "石のツルハシ x1" },
-];
+
+{
+name: "木のピッケル",
+recipe: [
+"木材 ×3",
+"棒 ×2"
+]
+},
+
+{
+name: "石のピッケル",
+recipe: [
+"石 ×3",
+"棒 ×2"
+]
+},
+
+{
+name: "鉄のピッケル",
+recipe:
