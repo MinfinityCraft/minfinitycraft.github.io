e4e4e032b18113df4d37039e233c90b41ec7fc1c
MinfinityCraft <yuzumikan773@gmail.com>
2026-05-15 00:27:14 +0900

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -224,3 +224,44 @@ document
     );
 
   });
+
+/* =========================
+   🌌 ページ遷移
+========================= */
+
+document
+  .querySelectorAll("a")
+  .forEach(link => {
+
+    link.addEventListener(
+      "click",
+      function(e) {
+
+        const href =
+          this.getAttribute("href");
+
+        if (
+          href &&
+          !href.startsWith("#") &&
+          !href.startsWith("http")
+        ) {
+
+          e.preventDefault();
+
+          document.body
+            .classList
+            .add("fade-out");
+
+          setTimeout(() => {
+
+            window.location.href =
+              href;
+
+          }, 500);
+
+        }
+
+      }
+    );
+
+  });
