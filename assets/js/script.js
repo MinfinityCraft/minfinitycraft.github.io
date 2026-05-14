3459531e369ff64ae87226f0ba646c775cb29fec
MinfinityCraft <yuzumikan773@gmail.com>
2026-05-14 10:14:17 +0900

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
 function toggleMenu() {
   document.querySelector(".sidebar").classList.toggle("active");
 }
+
+const links = document.querySelectorAll(".sidebar a");
+const current = window.location.pathname.split("/").pop();
+
+links.forEach(link => {
+  if (link.getAttribute("href") === current) {
+    link.classList.add("active");
+  }
+});
