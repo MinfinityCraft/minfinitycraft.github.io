ee1aa7d7f3209b23e510aba96815d7ca931fd6da
MinfinityCraft <yuzumikan773@gmail.com>
2026-06-15 11:39:10 +0900

    Update rules.js

--- assets/js/rules.js
+++ assets/js/rules.js
@@ -1,15 +1,41 @@
-
-console.log("START");
-
 async function loadRules() {
+
   const res = await fetch("assets/data/rules.json");
   const data = await res.json();
 
   const container = document.getElementById("rulesContainer");
+  container.innerHTML = "";
 
-  container.innerHTML = "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
+  for (const [title, ruleData] of Object.entries(data)) {
 
-  console.log("END");
+    const details = document.createElement("details");
+
+    let levelsHTML = "";
+
+    ruleData.levels.forEach(level => {
+
+      levelsHTML += `
+        <h3>${level.title}</h3>
+        <ul>
+          ${level.rules.map(r => `<li>${r}</li>`).join("")}
+        </ul>
+      `;
+    });
+
+    details.innerHTML = `
+      <summary>${title}</summary>
+
+      <div class="drop-data">
+
+        ${ruleData.description.map(d => `<p>${d}</p>`).join("")}
+
+        ${levelsHTML}
+
+      </div>
+    `;
+
+    container.appendChild(details);
+  }
 }
 
 loadRules();
